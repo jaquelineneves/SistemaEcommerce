dcaa7a8117028ed324fe0f308712b233575a4b40
ADMIN-PC\ADMIN <jaque.nevees2@gmail.com>
2019-03-06 16:58:52 -0300

    Criação da Classe Cliente e do BD

---
+++ src/model/domain/Fornecedor.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.domain;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Fornecedor {
+    private Integer codigo;
+    private String nome;
+    
+    public Fornecedor(){
+    
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 67 * hash + Objects.hashCode(this.codigo);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Fornecedor other = (Fornecedor) obj;
+        if (!Objects.equals(this.codigo, other.codigo)) {
+            return false;
+        }
+        return true;
+    }
+    
+}
